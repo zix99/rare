a746d238c9b5e6509ce3ca35f1ffaaa9fbb7807b
Christopher LaPointe <cmlapointe11@gmail.com>
2019-10-10 22:37:54 -0400

    Missed a file

---
+++ pkg/expressions/errors.go
@@ -0,0 +1,9 @@
+package expressions
+
+const (
+	ErrorBucket      = "<BUCKET-ERROR>"
+	ErrorBucketSize  = "<BUCKET-SIZE>"
+	ErrorOutOfBounds = "<OOB>"
+	ErrorType        = "<BAD-TYPE>"
+	ErrorArgCount    = "<ARGN>"
+)
