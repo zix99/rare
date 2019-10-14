27963cca28eea7bc1d4fcfe9461e526840a989a2
Christopher LaPointe <cmlapointe11@gmail.com>
2019-10-13 23:25:17 -0400

    Test more numerical analysis

--- pkg/aggregation/numerical_test.go
+++ pkg/aggregation/numerical_test.go
@@ -19,6 +19,21 @@ func TestSimpleNumericalAggregation(t *testing.T) {
 
 	data := aggr.Analyze()
 
-	assert.Equal(t, 10.0, data.Mean)
-	assert.Equal(t, 10.0, data.Median)
+	assert.Equal(t, 10.0, data.Median())
+	assert.Equal(t, 10.0, data.Quantile(0.5))
+	assert.Equal(t, 5.0, data.Mode())
+}
+
+func TestSimpleMode(t *testing.T) {
+	aggr := NewNumericalAggregator()
+	aggr.Sample(5)
+	aggr.Sample(10)
+	aggr.Sample(15)
+	aggr.Sample(5)
+	aggr.Sample(10)
+	aggr.Sample(5)
+
+	data := aggr.Analyze()
+	assert.Equal(t, 5.0, data.Mode())
+	assert.Equal(t, 15.0, data.Quantile(0.9))
 }
