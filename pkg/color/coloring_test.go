bb52b9390612708a9b6547580079d08bbdda465e
Christopher LaPointe <cmlapointe11@gmail.com>
2019-11-13 20:20:51 -0500

    More color testing

--- pkg/color/coloring_test.go
+++ pkg/color/coloring_test.go
@@ -3,7 +3,14 @@ package color
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func init() {
+	// Force enable, otherwise its init might not be
+	Enabled = true
+}
 
 func BenchmarkColorReplacer(b *testing.B) {
 	s := "This is a test"
@@ -15,4 +22,24 @@ func BenchmarkColorReplacer(b *testing.B) {
 	}
 
 	fmt.Println(out)
+}
+
+func TestWrap(t *testing.T) {
+	s := Wrap(Red, "hello")
+	assert.Contains(t, s, Red)
+	assert.Contains(t, s, "hello")
+}
+
+func TestWrapDisabled(t *testing.T) {
+	Enabled = false
+	s := Wrap(Red, "hello")
+	assert.Equal(t, "hello", s)
+	assert.NotContains(t, s, Red)
+	Enabled = true
+}
+
+func TestWrapf(t *testing.T) {
+	s := Wrapf(Green, "This is %d", 123)
+	assert.Contains(t, s, "This is 123")
+	assert.Contains(t, s, Green)
 }
