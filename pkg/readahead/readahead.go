273a494c7299b786c3c33a33cafe2b242e26c8ee
Christopher LaPointe <cmlapointe11@gmail.com>
2019-11-16 11:33:35 -0500

    Further optimize readahead

--- pkg/readahead/readahead.go
+++ pkg/readahead/readahead.go
@@ -29,6 +29,7 @@ type ReadAhead struct {
 	eof    bool
 
 	token []byte
+	delim byte
 }
 
 // dropCR drops a terminal \r from the data.
@@ -53,45 +54,15 @@ func New(reader io.Reader, maxBufLen int) *ReadAhead {
 	return &ReadAhead{
 		r:         reader,
 		maxBufLen: maxBufLen,
+		delim:     '\n',
 	}
 }
 
+// Scan for the next token with a new line
 func (s *ReadAhead) Scan() bool {
-	const averageLineLen = 512
-
-	if !s.eof && (s.buf == nil || s.offset > len(s.buf)-averageLineLen) {
-		// Need some more data in the buffer, but want to keep the existing one in-tact
-		// This will lead to a small amount of duplication by copying the remaining data
-		// in the old buf to the new buf
-		oldbuf := s.buf
-		s.buf = make([]byte, maxi(s.maxBufLen, len(oldbuf)-s.offset+s.maxBufLen/2))
-		startRead := 0
-
-		if oldbuf != nil && s.offset < len(oldbuf) {
-			// Copy end of old buf over, and populate the rest of the buffer from reader
-			startRead = len(oldbuf) - s.offset
-			copy(s.buf, oldbuf[s.offset:])
-		} else {
-			// Brand new data!
-			s.offset = 0
-		}
-
-		// Fill buffer
-		for startRead < len(s.buf) {
-			n, err := s.r.Read(s.buf[startRead:])
-			startRead += n
-			if err != nil {
-				s.eof = true
-				break
-			}
-		}
-
-		s.buf = s.buf[:startRead]
-		s.offset = 0
-	}
-
 	for {
-		relIndex := bytes.IndexByte(s.buf[s.offset:], '\n')
+		//var a chars
+		relIndex := bytes.IndexByte(s.buf[s.offset:], s.delim)
 
 		if relIndex >= 0 {
 			start := s.offset
@@ -102,34 +73,45 @@ func (s *ReadAhead) Scan() bool {
 
 		// No new line, so either:
 		// A) There's not enough room in the buffer
-		// B) We're towards the end of buf, and need more data (Though hopefully we pre-empted above)
-		if relIndex < 0 {
-			// Eof, so the rest of it will count as a line
-			if s.eof && s.offset < len(s.buf) {
-				ret := s.buf[s.offset:]
-				s.offset = len(s.buf)
-				s.token = ret
-				return true
-			} else if !s.eof {
-				// Not enough in buffer to find next new-line.. need to fill until finding
-				oldbuf := s.buf
-				s.buf = make([]byte, len(s.buf)+averageLineLen)
-				copy(s.buf, oldbuf)
+		// B) We're towards the end of buf, and need more data
 
-				n, err := s.r.Read(s.buf[len(oldbuf):])
-				s.buf = s.buf[:len(oldbuf)+n]
+		// Eof, so the rest of it will count as a line
+		if s.eof && s.offset < len(s.buf) {
+			ret := s.buf[s.offset:]
+			s.offset = len(s.buf)
+			s.token = ret
+			return true
+		} else if !s.eof {
+			// Not enough in buffer to find next new-line.. need to fill until finding
 
+			// Resize buffer, and copy over old buffer data
+			oldbuf := s.buf
+			s.buf = make([]byte, maxi(s.maxBufLen, len(oldbuf)-s.offset+s.maxBufLen/2))
+			copy(s.buf, oldbuf[s.offset:])
+			readOffset := len(oldbuf) - s.offset
+
+			// Fill remaining buffer
+			for readOffset < len(s.buf) {
+				n, err := s.r.Read(s.buf[readOffset:])
+				readOffset += n
 				if err != nil {
 					s.eof = true
+					break
 				}
-			} else {
-				s.token = nil
-				return false
 			}
+
+			// Trim buffer to read-length, and reset offset
+			s.buf = s.buf[:readOffset]
+			s.offset = 0
+		} else {
+			s.token = nil
+			return false
 		}
+
 	}
 }
 
+// Bytes retrieves the current bytes of the current token (line)
 func (s *ReadAhead) Bytes() []byte {
 	return s.token
 }
