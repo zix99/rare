33ccfb84f80bf074d43ea36088883b493bb8a8f8
Christopher LaPointe <cmlapointe11@gmail.com>
2019-11-23 13:59:19 -0500

    Move summary code

---
+++ cmd/helpers/summary.go
@@ -0,0 +1,26 @@
+package helpers
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"rare/pkg/color"
+	"rare/pkg/extractor"
+	"rare/pkg/humanize"
+)
+
+func FWriteExtractorSummary(extractor *extractor.Extractor) string {
+	var w bytes.Buffer
+	fmt.Fprintf(&w, "Matched: %s / %s",
+		color.Wrapi(color.BrightGreen, humanize.Hi(extractor.MatchedLines())),
+		color.Wrapi(color.BrightWhite, humanize.Hi(extractor.ReadLines())))
+	if extractor.IgnoredLines() > 0 {
+		fmt.Fprintf(&w, " (Ignored: %s)", color.Wrapi(color.Red, humanize.Hi(extractor.IgnoredLines())))
+	}
+	return w.String()
+}
+
+func WriteExtractorSummary(extractor *extractor.Extractor) {
+	os.Stderr.WriteString(FWriteExtractorSummary(extractor))
+	os.Stderr.WriteString("\n")
+}
