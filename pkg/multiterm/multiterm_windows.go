11831ce4bf940cc5f85f601b4c1f5fbdcd53336b
Christopher LaPointe <cmlapointe11@gmail.com>
2019-11-24 16:57:06 -0500

    Support colors and output on windows

--- pkg/multiterm/multiterm_windows.go
+++ pkg/multiterm/multiterm_windows.go
@@ -1,15 +1,15 @@
 package multiterm
 
 import (
-	"os"
 	"golang.org/x/sys/windows"
+	"os"
 )
 
 func addFlagToWindowsTerm(file *os.File) error {
 	term := windows.Handle(file.Fd())
 	var outMode uint32
 	if err := windows.GetConsoleMode(term, &outMode); err == nil {
-		if err := windows.SetConsoleMode(term, outMode | windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING); err != nil {
+		if err := windows.SetConsoleMode(term, outMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING); err != nil {
 			return err
 		}
 	} else {
