991fb9b8852a4af8eae9578c9b6034007b527b90
Christopher LaPointe <cmlapointe11@gmail.com>
2019-11-27 13:13:51 -0500

    A little file organization

--- cmd/helpers/readChannels.go
+++ cmd/helpers/readChannels.go
@@ -4,7 +4,6 @@ import (
 	"compress/gzip"
 	"io"
 	"os"
-	"path/filepath"
 	"rare/pkg/extractor"
 	"rare/pkg/readahead"
 	"sync"
@@ -105,38 +104,5 @@ func openFilesToChan(filenames []string, gunzip bool, concurrency int, batchSize
 		close(out)
 	}()
 
-	return out
-}
-
-func isDir(path string) bool {
-	if fi, err := os.Stat(path); err == nil && fi.IsDir() {
-		return true
-	}
-	return false
-}
-
-// globExpand expands a directory-glob, and optionally recurses on it
-func globExpand(paths []string, recursive bool) []string {
-	out := make([]string, 0)
-	for _, p := range paths {
-		if recursive && isDir(p) {
-			filepath.Walk(p, func(walkPath string, info os.FileInfo, err error) error {
-				if err != nil {
-					return err
-				}
-				if !info.IsDir() {
-					out = append(out, walkPath)
-				}
-				return nil
-			})
-		} else {
-			expanded, err := filepath.Glob(p)
-			if err != nil {
-				ErrLog.Printf("Path error: %v\n", err)
-			} else {
-				out = append(out, expanded...)
-			}
-		}
-	}
 	return out
 }
