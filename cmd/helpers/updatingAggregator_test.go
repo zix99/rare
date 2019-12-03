8c6d690cd6b629c24fb5022d76d2f79e23e40991
Christopher LaPointe <cmlapointe11@gmail.com>
2019-12-02 19:13:21 -0500

    Cover updating aggregator

---
+++ cmd/helpers/updatingAggregator_test.go
@@ -0,0 +1,53 @@
+package helpers
+
+import (
+	"io/ioutil"
+	"rare/pkg/extractor"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testData = `abc 123
+def 245
+qqq 123
+xxx`
+
+type VirtualAggregator struct {
+	items []string
+}
+
+func (s *VirtualAggregator) Sample(element string) {
+	s.items = append(s.items, element)
+}
+
+func (s *VirtualAggregator) ParseErrors() uint64 {
+	return 0
+}
+
+func TestAggregationLoop(t *testing.T) {
+	// Build a real extractor
+	input := extractor.ConvertReaderToStringChan("test", ioutil.NopCloser(strings.NewReader(testData)), 1)
+	ex, err := extractor.New(input, &extractor.Config{
+		Regex:   `(\d+)`,
+		Extract: "val:{1}",
+		Workers: 1,
+	})
+	assert.NoError(t, err)
+
+	// Build a fake aggregator
+	agg := &VirtualAggregator{}
+
+	outputTriggered := 0
+	RunAggregationLoop(ex, agg, func() {
+		outputTriggered++
+	})
+
+	// Validation
+	assert.GreaterOrEqual(t, outputTriggered, 1)
+	assert.Equal(t, 3, len(agg.items))
+
+	// Also validate summary building since we have all the correct context
+	WriteExtractorSummary(ex)
+}
