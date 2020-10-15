01c0dcef920c0a4577794afddcf221c832165944
Christopher LaPointe <cmlapointe11@gmail.com>
2020-10-15 01:07:48 +0000

    Clean up code a bit, add ratio, add tests

--- pkg/fuzzy/sift4/simple.go
+++ pkg/fuzzy/sift4/simple.go
@@ -7,6 +7,13 @@ func max2i(a, b int) int {
 	return b
 }
 
+func DistanceStringRatio(s1, s2 string, maxOffset int) float32 {
+	if len(s1) == 0 && len(s2) == 0 {
+		return 1.0
+	}
+	return 1.0 - float32(DistanceString(s1, s2, maxOffset))/float32(max2i(len(s1), len(s2)))
+}
+
 func DistanceString(s1, s2 string, maxOffset int) int {
 	return Distance([]rune(s1), []rune(s2), maxOffset)
 }
@@ -30,33 +37,29 @@ func Distance(s1, s2 []rune, maxOffset int) int {
 	l1 := len(s1)
 	l2 := len(s2)
 
-	c1 := 0       //cursor for string 1
-	c2 := 0       //cursor for string 2
-	lcss := 0     //largest common subsequence
-	local_cs := 0 //local common substring
-	for {
-		if c1 >= l1 || c2 >= l2 {
-			break
-		}
-
+	c1 := 0    //cursor for string 1
+	c2 := 0    //cursor for string 2
+	lcss := 0  //largest common subsequence
+	loccs := 0 //local common substring
+	for c1 < l1 && c2 < l2 {
 		if s1[c1] == s2[c2] {
-			local_cs++
+			loccs++
 		} else {
-			lcss += local_cs
-			local_cs = 0
+			lcss += loccs
+			loccs = 0
 			if c1 != c2 {
 				c1 = max2i(c1, c2) //using max to bypass the need for computer transpositions ('ab' vs 'ba')
 				c2 = c1
 			}
 			for i := 0; i < maxOffset && (c1+i < l1 || c2+i < l2); i++ {
-				if c1+i < l1 && c2 < l2 && s1[c1+i] == s2[c2] {
+				if c1+i < l1 && s1[c1+i] == s2[c2] {
 					c1 += i
-					local_cs++
+					loccs++
 					break
 				}
-				if c1 < l1 && c2+i < l2 && s1[c1] == s2[c2+i] {
+				if c2+i < l2 && s1[c1] == s2[c2+i] {
 					c2 += i
-					local_cs++
+					loccs++
 					break
 				}
 			}
@@ -64,6 +67,6 @@ func Distance(s1, s2 []rune, maxOffset int) int {
 		c1++
 		c2++
 	}
-	lcss += local_cs
+	lcss += loccs
 	return max2i(l1, l2) - lcss
 }
--- pkg/fuzzy/sift4/simple_test.go
+++ pkg/fuzzy/sift4/simple_test.go
@@ -22,27 +22,44 @@ func TestCompletelyDifferent(t *testing.T) {
 	assert.Equal(t, 7, DistanceString("abcdefg", "1234567", 10))
 }
 
-/*
+func TestCompletelyDifferent2(t *testing.T) {
+	assert.Equal(t, 17, DistanceString("abcdefgjjjjjjjjjj", "1234567", 10))
+}
+
+func TestZeroLength(t *testing.T) {
+	assert.Equal(t, 0, DistanceString("", "", 10))
+	assert.Equal(t, 3, DistanceString("abc", "", 10))
+	assert.Equal(t, 3, DistanceString("", "abc", 10))
+}
+
 func TestCompletelyDifferentRatio(t *testing.T) {
-	assert.Equal(t, float32(0), DistanceStringRatio("abcdefg", "qqqqqqq"))
+	assert.Equal(t, float32(0), DistanceStringRatio("abcdefg", "qqqqqqq", 10))
 }
 
 func TestHalfSimilar(t *testing.T) {
-	assert.Equal(t, float32(0.5), DistanceStringRatio("abcdef", "qqqdef"))
+	assert.Equal(t, float32(0.5), DistanceStringRatio("abcdef", "qqqdef", 10))
 }
 
 func TestFullSimilar(t *testing.T) {
-	assert.Equal(t, float32(1.0), DistanceStringRatio("abc", "abc"))
-}*/
+	assert.Equal(t, float32(1.0), DistanceStringRatio("abc", "abc", 10))
+}
+
+func TestDifferentSimilar(t *testing.T) {
+	assert.Equal(t, float32(0.5), DistanceStringRatio("abc", "abcdef", 10))
+}
+
+func TestZeroSimilar(t *testing.T) {
+	assert.Equal(t, float32(1.0), DistanceStringRatio("", "", 10))
+}
 
 func BenchmarkSimilarityHigh(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		DistanceString("this is a very long string to test with", "this is a very short string to test with", 100)
+		DistanceString("this is a very long string to test with", "this is a very short string to test with", 10)
 	}
 }
 
 func BenchmarkSimilarityLow(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		DistanceString("this is a very long string to test with", "a completely different string with a few similar words", 5)
+		DistanceString("this is a very long string to test with", "a completely different string with a few similar words", 10)
 	}
 }
