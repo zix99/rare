614dfa986f35acc864b66e6be2ba17f0a2121513
Christopher LaPointe <cmlapointe11@gmail.com>
2022-08-25 22:04:38 -0400

    Sort tests

---
+++ pkg/aggregation/sorting/namevalue_test.go
@@ -0,0 +1,53 @@
+package sorting
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type pair struct {
+	s string
+	v int64
+}
+
+func (s pair) SortName() string {
+	return s.s
+}
+func (s pair) SortValue() int64 {
+	return s.v
+}
+
+func TestNameValueSorter(t *testing.T) {
+	arr := []pair{
+		{"b", 123},
+		{"q", 44},
+		{"a", 44},
+	}
+
+	SortNameValue(arr, ValueSorter())
+
+	expected := []pair{
+		{"b", 123},
+		{"a", 44},
+		{"q", 44},
+	}
+	assert.Equal(t, expected, arr)
+}
+
+func TestNameValueNilSorter(t *testing.T) {
+	arr := []pair{
+		{"b", 123},
+		{"q", 44},
+		{"a", 44},
+	}
+
+	SortNameValue(arr, ValueNilSorter(ByName))
+
+	expected := []pair{
+		{"a", 44},
+		{"b", 123},
+		{"q", 44},
+	}
+	assert.Equal(t, expected, arr)
+}
--- pkg/aggregation/sorting/strings_test.go
+++ pkg/aggregation/sorting/strings_test.go
@@ -2,7 +2,41 @@ package sorting
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestNameSort(t *testing.T) {
+	assert.True(t, ByName("a", "b"))
+}
+
+func TestNameSmartSort(t *testing.T) {
+	assert.True(t, ByNameSmart("a", "b"))
+	assert.True(t, ByNameSmart("0.0", "1.0"))
+	assert.True(t, ByNameSmart("1", "b"))
+}
+
+func TestSortStrings(t *testing.T) {
+	arr := []string{"b", "c", "a", "q"}
+	SortStrings(arr, ByNameSmart)
+	assert.Equal(t, []string{"a", "b", "c", "q"}, arr)
+}
+
+func TestSortStringsBy(t *testing.T) {
+	type wrapper struct {
+		s string
+	}
+	arr := []wrapper{
+		{"b"},
+		{"a"},
+		{"c"},
+	}
+	SortStringsBy(arr, ByName, func(w wrapper) string { return w.s })
+
+	assert.Equal(t, "a", arr[0].s)
+	assert.Equal(t, "b", arr[1].s)
+	assert.Equal(t, "c", arr[2].s)
+}
 
 // wrapped BenchmarkStringSort-4   	 6859735	       177.0 ns/op	      32 B/op	       1 allocs/op
 func BenchmarkStringSort(b *testing.B) {
