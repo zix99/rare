a468bdb72ce003425bb218d1f33dfe8b4a4a50af
Christopher LaPointe <cmlapointe11@gmail.com>
2023-06-11 12:05:59 -0400

    Increase test coverage

--- cmd/helpers/output_test.go
+++ cmd/helpers/output_test.go
@@ -17,29 +17,27 @@ func TestBuildVTerm(t *testing.T) {
 func TestBuildVTermFromArgs(t *testing.T) {
 	app := cli.NewApp()
 	app.Flags = []cli.Flag{
-		&cli.BoolFlag{
-			Name: "snapshot",
-		},
+		SnapshotFlag,
+		NoOutFlag,
 	}
 	app.Action = func(ctx *cli.Context) error {
 		BuildVTermFromArguments(ctx)
 		return nil
 	}
 	assert.NoError(t, app.Run([]string{"", "--snapshot"}))
+	assert.NoError(t, app.Run([]string{"", "--noout"}))
 }
 
 func TestTryWriteCSV(t *testing.T) {
 	app := cli.NewApp()
 	app.Flags = []cli.Flag{
-		&cli.StringFlag{
-			Name: "csv",
-		},
+		CSVFlag,
 	}
 	app.Action = func(ctx *cli.Context) error {
 		agg := aggregation.NewCounter()
-		TryWriteCSV(ctx, agg, csv.WriteCounter)
-		return nil
+		return TryWriteCSV(ctx, agg, csv.WriteCounter)
 	}
 	assert.NoError(t, app.Run([]string{""}))
 	assert.NoError(t, app.Run([]string{"", "--csv", "-"}))
+	assert.Error(t, app.Run([]string{"", "--csv", "/!@#bad-filename"}))
 }
