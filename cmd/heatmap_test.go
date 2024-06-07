44f65bd9dd80c5ccad915640d22e1e0189ff1ef9
Christopher LaPointe <cmlapointe11@gmail.com>
2024-06-06 20:55:14 -0400

    Fix non-deterministic test

--- cmd/heatmap_test.go
+++ cmd/heatmap_test.go
@@ -17,12 +17,12 @@ func TestHeatmapLinear(t *testing.T) {
 	out, eout, err := testCommandCapture(heatmapCommand(), `--snapshot -m "(.+) (.+)" -e "{$ {1} {2}}" testdata/heat.txt`)
 	assert.NoError(t, err)
 	assert.Empty(t, eout)
-	assert.Equal(t, " - 0    2 1    4 2    6 3    9 4\n a..\nx -22\ny 224\nz 9--\nMatched: 10 / 10 (R: 3; C: 3)\n39 B (0 B/s) \n", out)
+	assert.Contains(t, out, " - 0    2 1    4 2    6 3    9 4\n a..\nx -22\ny 224\nz 9--\nMatched: 10 / 10 (R: 3; C: 3)\n39 B (0 B/s)")
 }
 
 func TestHeatmapLog2(t *testing.T) {
 	out, eout, err := testCommandCapture(heatmapCommand(), `--snapshot -m "(.+) (.+)" -e "{$ {1} {2}}" --scale log2 testdata/heat.txt`)
 	assert.NoError(t, err)
 	assert.Empty(t, eout)
-	assert.Equal(t, " - 1    4 2    7 3    9 4\n a..\nx ---\ny --4\nz 9--\nMatched: 10 / 10 (R: 3; C: 3)\n39 B (0 B/s) \n", out)
+	assert.Contains(t, out, " - 1    4 2    7 3    9 4\n a..\nx ---\ny --4\nz 9--\nMatched: 10 / 10 (R: 3; C: 3)\n39 B (0 B/s)")
 }
