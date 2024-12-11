3c01f63d20c5209c4141de47567de95f2cc5cbb0
Christopher LaPointe <cmlapointe11@gmail.com>
2024-12-10 22:19:22 -0500

    Test all docs compile correctly

--- cmd/docs_test.go
+++ cmd/docs_test.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"bytes"
+	"rare/docs"
+	"rare/pkg/markdowncli"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,6 +12,27 @@ import (
 func TestDocs(t *testing.T) {
 	assert.NoError(t, testCommand(docsCommand(), ``))
 	assert.NoError(t, testCommand(docsCommand(), `-n expressions`))
-	assert.NoError(t, testCommand(docsCommand(), `-n exp`))
+	assert.NoError(t, testCommand(docsCommand(), `-n exp`)) // partial name
 	assert.Error(t, testCommand(docsCommand(), `no-exist`))
+}
+
+// Test that all the docs make it through markdown parser with a result
+func TestDocsProcess(t *testing.T) {
+	entries, _ := docs.DocFS.ReadDir(docs.BasePath)
+	assert.NotZero(t, len(entries))
+
+	for _, entry := range entries {
+		entry := entry
+		t.Run(entry.Name(), func(t *testing.T) {
+			t.Parallel()
+			f, err := docs.DocFS.Open(docs.BasePath + "/" + entry.Name())
+			assert.NoError(t, err)
+			defer f.Close()
+
+			var buf bytes.Buffer
+			markdowncli.WriteMarkdownToBuf(&buf, f)
+
+			assert.NotZero(t, buf.Len())
+		})
+	}
 }
