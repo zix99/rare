6e4243be573c1e45f5a8fb943ffaf99ab839ca76
Christopher LaPointe <cmlapointe11@gmail.com>
2025-01-11 20:55:12 -0500

    Remove memory alloc from dissect, 50% improvement

--- pkg/matchers/dissect/dissect_test.go
+++ pkg/matchers/dissect/dissect_test.go
@@ -9,8 +9,9 @@ import (
 func TestDissectBasic(t *testing.T) {
 	d := MustCompile("%{val};%{};%{?skip} - %{val2}").CreateInstance()
 
-	assert.Equal(t, []int{0, 17, 0, 5, 12, 17}, d.FindSubmatchIndex([]byte("Hello;a;b - there")))
+	assert.Equal(t, []int{0, 17, 0, 5, 12, 17}, d.FindSubmatchIndexDst([]byte("Hello;a;b - there"), nil))
 
+	assert.Equal(t, 6, d.MatchBufSize())
 	assert.Equal(t, map[string]int{
 		"val":  1,
 		"val2": 2,
@@ -18,9 +19,10 @@ func TestDissectBasic(t *testing.T) {
 }
 func TestUtf8(t *testing.T) {
 	d := MustCompile("ûɾ %{key} ḝłįʈ").CreateInstance()
+	assert.Equal(t, 4, d.MatchBufSize())
 
 	s := []byte("Ḽơᶉëᶆ ȋṕšᶙṁ ḍỡḽǭᵳ ʂǐť ӓṁệẗ, ĉṓɲṩḙċťᶒțûɾ ấɖḯƥĭṩčįɳġ ḝłįʈ, șếᶑ ᶁⱺ ẽḭŭŝḿꝋď ṫĕᶆᶈṓɍ ỉñḉīḑȋᵭṵńť ṷŧ ḹẩḇőꝛế éȶ đꝍꞎôꝛȇ ᵯáꞡᶇā ąⱡîɋṹẵ.")
-	m := d.FindSubmatchIndex(s)
+	m := d.FindSubmatchIndexDst(s, nil)
 	assert.Equal(t, []int{85, 123, 90, 113}, m)
 	assert.Equal(t, []byte("ûɾ ấɖḯƥĭṩčįɳġ ḝłįʈ"), s[m[0]:m[1]])
 	assert.Equal(t, []byte("ấɖḯƥĭṩčįɳġ"), s[m[2]:m[3]])
@@ -29,53 +31,56 @@ func TestUtf8(t *testing.T) {
 func TestPrefixOnSkipKey(t *testing.T) {
 	d := MustCompile("prefix %{}: %{val}").CreateInstance()
 
-	assert.Nil(t, d.FindSubmatchIndex([]byte("a: b")))
-	assert.Equal(t, []int{0, 11, 10, 11}, d.FindSubmatchIndex([]byte("prefix a: b")))
-	assert.Nil(t, d.FindSubmatchIndex([]byte("Prefix a: b")))
+	assert.Nil(t, d.FindSubmatchIndexDst([]byte("a: b"), nil))
+	assert.Equal(t, []int{0, 11, 10, 11}, d.FindSubmatchIndexDst([]byte("prefix a: b"), nil))
+	assert.Nil(t, d.FindSubmatchIndexDst([]byte("Prefix a: b"), nil))
 }
 
 func TestEmpty(t *testing.T) {
 	d := MustCompile("").CreateInstance()
-	assert.Equal(t, []int{0, 0}, d.FindSubmatchIndex([]byte("hello")))
-	assert.Equal(t, []int{0, 0}, d.FindSubmatchIndex([]byte("")))
+	assert.Equal(t, 2, d.MatchBufSize())
+	assert.Equal(t, []int{0, 0}, d.FindSubmatchIndexDst([]byte("hello"), nil))
+	assert.Equal(t, []int{0, 0}, d.FindSubmatchIndexDst([]byte(""), nil))
 }
 
 func TestNoTokens(t *testing.T) {
 	d := MustCompile("test").CreateInstance()
+	assert.Equal(t, 2, d.MatchBufSize())
 
-	assert.Nil(t, d.FindSubmatchIndex([]byte("hello there")))
-	assert.Equal(t, []int{0, 4}, d.FindSubmatchIndex([]byte("test")))
-	assert.Equal(t, []int{1, 5}, d.FindSubmatchIndex([]byte("atest")))
-	assert.Equal(t, []int{3, 7}, d.FindSubmatchIndex([]byte("abctest")))
-	assert.Equal(t, []int{0, 4}, d.FindSubmatchIndex([]byte("testa")))
-	assert.Equal(t, []int{0, 4}, d.FindSubmatchIndex([]byte("testabc")))
-	assert.Equal(t, []int{3, 7}, d.FindSubmatchIndex([]byte("abctestabc")))
-	assert.Nil(t, d.FindSubmatchIndex([]byte("tEst")))
+	assert.Nil(t, d.FindSubmatchIndexDst([]byte("hello there"), nil))
+	assert.Equal(t, []int{0, 4}, d.FindSubmatchIndexDst([]byte("test"), nil))
+	assert.Equal(t, []int{1, 5}, d.FindSubmatchIndexDst([]byte("atest"), nil))
+	assert.Equal(t, []int{3, 7}, d.FindSubmatchIndexDst([]byte("abctest"), nil))
+	assert.Equal(t, []int{0, 4}, d.FindSubmatchIndexDst([]byte("testa"), nil))
+	assert.Equal(t, []int{0, 4}, d.FindSubmatchIndexDst([]byte("testabc"), nil))
+	assert.Equal(t, []int{3, 7}, d.FindSubmatchIndexDst([]byte("abctestabc"), nil))
+	assert.Nil(t, d.FindSubmatchIndexDst([]byte("tEst"), nil))
 }
 
 func TestPrefix(t *testing.T) {
 	d := MustCompile("mid %{val};%{val2} after").CreateInstance()
 
-	assert.Equal(t, []int{12, 29, 16, 19, 20, 23}, d.FindSubmatchIndex([]byte("string with mid 123;456 after k")))
-	assert.Nil(t, d.FindSubmatchIndex([]byte("string with mi 123;456 after k")))
-	assert.Nil(t, d.FindSubmatchIndex([]byte("string with mid 123;456 boom k")))
-	assert.Nil(t, d.FindSubmatchIndex([]byte("")))
+	assert.Equal(t, []int{12, 29, 16, 19, 20, 23}, d.FindSubmatchIndexDst([]byte("string with mid 123;456 after k"), nil))
+	assert.Nil(t, d.FindSubmatchIndexDst([]byte("string with mi 123;456 after k"), nil))
+	assert.Nil(t, d.FindSubmatchIndexDst([]byte("string with mid 123;456 boom k"), nil))
+	assert.Nil(t, d.FindSubmatchIndexDst([]byte(""), nil))
 }
 
 func TestSuffix(t *testing.T) {
 	d := MustCompile("%{val};%{val2} after").CreateInstance()
 
-	assert.Equal(t, []int{0, 13, 0, 3, 4, 7}, d.FindSubmatchIndex([]byte("123;456 after k")))
-	assert.Equal(t, []int{0, 17, 0, 7, 8, 11}, d.FindSubmatchIndex([]byte("hah 123;456 after k")))
-	assert.Nil(t, d.FindSubmatchIndex([]byte("123;456 boom k")))
-	assert.Nil(t, d.FindSubmatchIndex([]byte("")))
+	assert.Equal(t, []int{0, 13, 0, 3, 4, 7}, d.FindSubmatchIndexDst([]byte("123;456 after k"), nil))
+	assert.Equal(t, []int{0, 17, 0, 7, 8, 11}, d.FindSubmatchIndexDst([]byte("hah 123;456 after k"), nil))
+	assert.Nil(t, d.FindSubmatchIndexDst([]byte("123;456 boom k"), nil))
+	assert.Nil(t, d.FindSubmatchIndexDst([]byte(""), nil))
 
-	assert.Equal(t, []int{2, 13, 6, 13}, MustCompile("end %{nada}").CreateInstance().FindSubmatchIndex([]byte("a end nothing")))
+	assert.Equal(t, []int{2, 13, 6, 13}, MustCompile("end %{nada}").CreateInstance().FindSubmatchIndexDst([]byte("a end nothing"), nil))
 }
 
 func TestNoPrefixSuffix(t *testing.T) {
 	d := MustCompile("%{onlymatch}").CreateInstance()
-	assert.Equal(t, []int{0, 5, 0, 5}, d.FindSubmatchIndex([]byte("a b c")))
+	assert.Equal(t, 4, d.MatchBufSize())
+	assert.Equal(t, []int{0, 5, 0, 5}, d.FindSubmatchIndexDst([]byte("a b c"), nil))
 }
 
 func TestErrorNew(t *testing.T) {
@@ -102,24 +107,31 @@ func TestIgnoreCase(t *testing.T) {
 	d, err := CompileEx("TeSt1", true)
 
 	assert.NoError(t, err)
-	assert.Equal(t, []int{0, 5}, d.CreateInstance().FindSubmatchIndex([]byte("test1")))
-	assert.Equal(t, []int{0, 5}, d.CreateInstance().FindSubmatchIndex([]byte("tEst1")))
-	assert.Equal(t, []int{0, 5}, d.CreateInstance().FindSubmatchIndex([]byte("TEST1")))
-	assert.Equal(t, []int{1, 6}, d.CreateInstance().FindSubmatchIndex([]byte("ATest123")))
-	assert.Nil(t, d.CreateInstance().FindSubmatchIndex([]byte("asdf")))
+	assert.Equal(t, []int{0, 5}, d.CreateInstance().FindSubmatchIndexDst([]byte("test1"), nil))
+	assert.Equal(t, []int{0, 5}, d.CreateInstance().FindSubmatchIndexDst([]byte("tEst1"), nil))
+	assert.Equal(t, []int{0, 5}, d.CreateInstance().FindSubmatchIndexDst([]byte("TEST1"), nil))
+	assert.Equal(t, []int{1, 6}, d.CreateInstance().FindSubmatchIndexDst([]byte("ATest123"), nil))
+	assert.Nil(t, d.CreateInstance().FindSubmatchIndexDst([]byte("asdf"), nil))
 
 	d, err = CompileEx("pref %{val} post", true)
 	assert.NoError(t, err)
-	assert.Equal(t, []int{2, 13, 7, 8}, d.CreateInstance().FindSubmatchIndex([]byte("a Pref 5 pOst")))
+	assert.Equal(t, []int{2, 13, 7, 8}, d.CreateInstance().FindSubmatchIndexDst([]byte("a Pref 5 pOst"), nil))
 }
 
-// BenchmarkDissect-4   	13347456	        86.07 ns/op	      32 B/op	       0 allocs/op
+func TestZeroAlloc(t *testing.T) {
+	b := testing.Benchmark(BenchmarkDissect)
+	assert.Zero(t, b.AllocedBytesPerOp())
+	assert.Zero(t, b.AllocsPerOp())
+}
+
+// BenchmarkDissect-4   	24508795	        44.94 ns/op	       0 B/op	       0 allocs/op
 func BenchmarkDissect(b *testing.B) {
 	d, _ := CompileEx("t%{val} ", false)
 	di := d.CreateInstance()
 	val := []byte("this is a test ")
+	buf := make([]int, 0, d.MatchBufSize())
 
 	for i := 0; i < b.N; i++ {
-		di.FindSubmatchIndex(val)
+		di.FindSubmatchIndexDst(val, buf)
 	}
 }
