7c1108a25ea0f4174e47d1b8366f3b8468962986
Christopher LaPointe <cmlapointe11@gmail.com>
2025-04-25 20:48:58 -0400

    More tests

--- pkg/matchers/fastregex/re2_test.go
+++ pkg/matchers/fastregex/re2_test.go
@@ -31,18 +31,43 @@ func TestMemoryAssumptions(t *testing.T) {
 	r := MustCompile(`t(\w+)`)
 	str := []byte("this is a test")
 	ri := r.CreateInstance()
-	buf := make([]int, 0, ri.MatchBufSize())
 
 	// Same memory
-	ret := ri.FindSubmatchIndexDst(str, buf)
-	assert.Equal(t, []int{0, 4, 1, 4}, ret)
-	testutil.AssertSameMemory(t, ret, buf)
+	t.Run("same memory", func(t *testing.T) {
+		buf := make([]int, 0, ri.MatchBufSize())
+		ret := ri.FindSubmatchIndexDst(str, buf)
+		assert.Equal(t, []int{0, 4, 1, 4}, ret)
+		testutil.AssertSameMemory(t, ret, buf)
+	})
 
 	// undersized alloc
-	buf = make([]int, 0, 2)
-	ret = ri.FindSubmatchIndexDst(str, buf)
-	assert.Equal(t, []int{0, 4, 1, 4}, ret)
-	testutil.AssertNotSameMemory(t, ret, buf)
+	t.Run("undersized", func(t *testing.T) {
+		buf := make([]int, 0, 2)
+		ret := ri.FindSubmatchIndexDst(str, buf)
+		assert.Equal(t, []int{0, 4, 1, 4}, ret)
+		testutil.AssertNotSameMemory(t, ret, buf)
+	})
+
+	// Nil buf alloc
+	t.Run("nilbuf", func(t *testing.T) {
+		ret := ri.FindSubmatchIndexDst(str, nil)
+		assert.Equal(t, []int{0, 4, 1, 4}, ret)
+	})
+
+	// No match
+	t.Run("no match", func(t *testing.T) {
+		ret := ri.FindSubmatchIndexDst([]byte("none"), nil)
+		assert.Nil(t, ret)
+	})
+
+	// pad match
+	t.Run("pad match", func(t *testing.T) {
+		r := MustCompile(`(\d+)(?: (\d+))?`).CreateInstance()
+		buf := make([]int, 0, r.MatchBufSize())
+		ret := r.FindSubmatchIndexDst([]byte("123"), buf)
+		assert.Equal(t, []int{0, 3, 0, 3, -1, -1}, ret)
+		testutil.AssertSameMemory(t, buf, ret)
+	})
 }
 
 func TestDstZeroAlloc(t *testing.T) {
