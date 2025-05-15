f22a0d7603812a94d523b00c3deb3f11aebdaa9c
Christopher LaPointe <cmlapointe11@gmail.com>
2025-05-15 10:49:53 -0400

    Move test for mount

---
+++ pkg/extractor/dirwalk/platform_unix_test.go
@@ -0,0 +1,12 @@
+package dirwalk
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsDifferentMount(t *testing.T) {
+	assert.False(t, isDifferentMount("/etc"))
+	assert.True(t, isDifferentMount("/proc"))
+}
