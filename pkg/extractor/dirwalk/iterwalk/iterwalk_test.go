899158c96ddea2887b2518bb6f60c09c48e7c690
Christopher LaPointe <cmlapointe11@gmail.com>
2025-05-31 22:09:15 -0400

    Test bugfix

--- pkg/extractor/dirwalk/iterwalk/iterwalk_test.go
+++ pkg/extractor/dirwalk/iterwalk/iterwalk_test.go
@@ -4,6 +4,7 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -68,6 +69,38 @@ func TestWalkDir_SkipDir(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Contains(t, walked, sub)
 	assert.NotContains(t, walked, filepath.Join(sub, "file.txt"))
+}
+
+func TestWalkDir_SkipDir_Large(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	dir := t.TempDir()
+	sub := filepath.Join(dir, "bigsubdir")
+	os.Mkdir(sub, 0755)
+	// Create many files in the subdirectory
+	for i := range readBatchSize + 1 {
+		os.WriteFile(filepath.Join(sub, "file_"+strconv.Itoa(i)+".txt"), []byte("x"), 0644)
+	}
+
+	var walked []string
+	err := WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		if d != nil && !d.IsDir() {
+			return filepath.SkipDir
+		}
+		if !d.IsDir() {
+			walked = append(walked, path)
+		}
+		return nil
+	})
+	assert.NoError(t, err)
+	assert.Len(t, walked, 0)
+
+	// None of the files in the skipped subdir should be walked
+	for i := 0; i < readBatchSize+1; i++ {
+		assert.NotContains(t, walked, filepath.Join(sub, "file_"+strconv.Itoa(i)+".txt"))
+	}
 }
 
 func TestWalkDir_Error(t *testing.T) {
